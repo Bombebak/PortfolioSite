a50af9e64c44f02af82ae9686fd1f0b22bae1d4b
Jakob Bak <Den_Farlige_Pilot@hotmail.com>
2018-08-01 22:13:21 +0200

    Removed old shit + added umbraco main site

--- PortfolioApp/PortfolioApp/Content/Js/App/Lottery/lotteryMod.js
+++
@@ -1,4 +0,0 @@
-﻿//https://blog.hfarazm.com/data-binding-angularjs/
-//https://www.c-sharpcorner.com/uploadfile/302f8f/asp-net-mvc-5-with-angularjs-part-1/
-//http://www.dotnetcurry.com/aspnet-mvc/1000/angularjs-aspnet-mvc-beginner-tutorial
-//https://stackoverflow.com/questions/24316355/multiple-controllers-with-angularjs-in-single-page-app
--- PortfolioApp/PortfolioApp/obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PortfolioApp.MvcApplication" Language="C#" %>
